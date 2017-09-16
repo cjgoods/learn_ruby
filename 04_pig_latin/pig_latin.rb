2ec4e33b931691004ebf93fbaa0d8773e7744802
Chris Goodson <cjgoods@centos.goodsonimagery.local>
2017-09-16 00:39:48 -0400

    Completed 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,26 +1,52 @@
 #write your code here
-def translate(v)
+def translate(x)
   vowels = "aeiouy".split("")
-  consonants = "bcdfghjklmnpqrstvwxz".split("")
-  vowelIndex = 100
-  if vowels.include? v[0]
-    puts "Starts with Vowel"
-    "#{v}ay"
-  else
-    vowels.each do |i|
-      puts "Iterating through letter #{i}"
-      puts "Vowel Index for #{i} = " + v.index("#{i}").to_s
-      next if v.index("#{i}").nil?
-      if v.index("#{i}") < vowelIndex
-        vowelIndex = v.index("#{i}")
-        puts "New Index = " + vowelIndex.to_s
+
+  y = x.split(" ")
+  y.map! do |v|
+    vowelIndex = 100
+    if vowels.include? v[0]
+      if y[1].nil?
+        return "#{v}ay"
+      end
+      "#{v}ay"
+    else
+      vowels.each do |i|
+        next if v.index("#{i}").nil?
+        if v.index("#{i}") < vowelIndex
+          vowelIndex = v.index("#{i}")
+        end
+      end
+      if vowelIndex == 1
+        if y[1].nil?
+          if v[0..1] == "qu"
+            return "#{v[2..-1]}#{v[0..1]}ay"
+          else
+            "#{v[1..-1]}#{v[0]}ay"
+          end
+        else
+          if v[0..1] == "qu"
+            "#{v[2..-1]}#{v[0..1]}ay"
+          else
+            "#{v[1..-1]}#{v[0]}ay"
+          end
+        end
+      else
+        if y[1].nil?
+          if v[(vowelIndex-1)..vowelIndex] == "qu"
+            return "#{v[(vowelIndex+1)..-1]}#{v[0..vowelIndex]}ay"
+          else
+            "#{v[vowelIndex..-1]}#{v[0..(vowelIndex-1)]}ay"
+          end
+        else
+          if v[(vowelIndex-1)..vowelIndex] == "qu"
+            "#{v[(vowelIndex+1)..-1]}#{v[0..vowelIndex]}ay"
+          else
+            "#{v[vowelIndex..-1]}#{v[0..(vowelIndex-1)]}ay"
+          end
+        end
       end
     end
-    puts "Final Vowel Index = " + vowelIndex.to_s
-    if vowelIndex == 1
-      "#{v[1..-1]}#{v[0]}ay"
-    else
-      "#{v[vowelIndex..-1]}#{v[0..(vowelIndex-1)]}ay"
-    end
   end
+  y.join(" ")
 end
